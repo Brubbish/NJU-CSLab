b9a1cebb3312a3b0ffaba9e31fbdc129ffc395e9
tracer-ics2020 <tracer@njuics.org>
2021-03-29 23:28:48 +0800

    >  compile 191220000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 23c36a05418f4b3aa1ceb710b76c58c8            Boot ID: d8199b84b1654fb78d9034d6719ca35e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-16-amd64       Architecture: x86-64  23:28:48 up 10:42,  2 users,  load average: 0.07, 0.02, 0.00

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -8,8 +8,22 @@ const char *regsl[] = {
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
+
 void isa_reg_display() {
+  int i;
+  for (i = R_EAX; i <= R_EDI; i ++) {
+    printf("%-8s %-16x %-16d\n", regsl[i], reg_l(i), reg_l(i));
+  }
+
+  for (i = R_AX; i <= R_DI; i ++) {
+    printf("%-8s %-16x %-16d\n", regsw[i], reg_w(i), reg_w(i));
+  }
+
+  for (i = R_AL; i <= R_BH; i ++) {
+    printf("%-8s %-16x %-16d\n", regsb[i], reg_b(i), reg_b(i));
+  }
 }
+
 
 word_t isa_reg_str2val(const char *s, bool *success) {
   return 0;
--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -6,6 +6,8 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+#include "../../../include/memory/vaddr.h"
+
 void cpu_exec(uint64_t);
 int is_batch_mode();
 
@@ -39,6 +41,12 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+//add
+static int cmd_s(char *args);
+static int info(char *args);
+//static int cmd_x(char *args);
+
+
 static struct {
   char *name;
   char *description;
@@ -47,12 +55,76 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si [N]", "Step", cmd_s},
+  { "info SUBCMD", "info regs", info},
+//  { "x N EXPR", "x", cmd_x} ,
 
   /* TODO: Add more commands */
 
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
+
+//add
+static int cmd_s(char *args){
+	int n;
+	if( args == NULL)
+		n = 1;
+	else{
+		char *arg = strtok(NULL," ");
+		n = atoi(arg);
+	}
+	cpu_exec(n);
+	return 0;
+}
+
+static int info (char *args){
+
+	char *arg = strtok(NULL, " ");
+	if(!strcmp(arg,"r"))
+		isa_reg_display();
+//	else if(!strcmp(arg,"w"))
+//		isa_
+
+/*	if (args[0] == 'r'){
+		int i;
+		for(i = R_EAX; i <= R_EDI; i++){
+			printf("$%s\t0x%08x\n"),regsl[i], reg_l(i));
+		}
+		printf("$eip\t0x%08x\n", cpu.eip);
+	}
+*/
+	return 0;
+}
+
+
+/*
+static int cmd_x(char *args){
+	if(args == NULL){
+		printf("Wrong\n");
+		return 0;
+	}
+	int num,exprs;
+	sscanf(args, "%d%x", &num,&exprs);
+	int i;
+	for(i=0;i<num;i++){
+		printf("0x%8x   0x%x\n",exprs + i*32, vaddr_read(exprs + i*32,32));
+	}
+	return 0;
+
+
+
+	char *arg1 = strtok(NULL," ");
+	int n = atoi(arg1);
+	char *arg2 = strtok(NULL," ");
+	int expr = 0;
+	
+
+}
+*/
+
+
+
 
 static int cmd_help(char *args) {
   /* extract the first argument */
